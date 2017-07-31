8dcce0683faa28dc8c9b940f0d7924382d3e84b1
robttay <robttay@yahoo.com>
2017-07-31 16:08:51 -0500

    First Commit

---
+++ src/RunnableDemo.java
@@ -0,0 +1,29 @@
+import static com.sun.glass.ui.Application.run;
+
+public class RunnableDemo {
+    public static void main(String[] args) {
+
+        // !! - Write an anonymous Runnable class here
+        //It can just print out "Anonymous run" or similar
+        Runnable anonRun = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Anonymous run");
+            }
+        };
+               anonRun.run();
+
+
+
+
+//        // !! - Write a lambda function here
+//        //It can also print out something like "Lambda run"
+//        Runnable lambdaRun = // lambda run here
+
+
+        Runnable lambdaRun = () -> { System.out.println("Lambda run"); };
+                lambdaRun.run();
+
+    }
+}
+
